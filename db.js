c72d7185af16707c954e4c2aa2de2d7bec767665
tru879 <ukeytrupti0@gmail.com>
2024-06-06 19:45:09 +0530

    version2:fixed the err undefined error

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 //------------------------------------------
-//const mongoURL = process.env.MONGODB_URL_LOCAL;     //using variable from .env file
-const mongoURL = process.env.MONGODB_URL;           //using variable from .env file
+const mongoURL = process.env.MONGODB_URL_LOCAL;     //using variable from .env file
+//const mongoURL = process.env.MONGODB_URL;           //using variable from .env file
 
 //------------------------------------------
 mongoose.connect(mongoURL);         //establish connection
@@ -14,7 +14,7 @@ db.on('connected',()=>{
     console.log('connected to mongodb server');
 })
 db.on('error',()=>{
-    console.log('Mongodb connection error',err);
+    console.log('Mongodb connection error');
 })
 db.on('disconnected',()=>{
     console.log('disconnected to mongodb server');
